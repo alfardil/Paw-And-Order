4dff1193a72aa3808d092d6d98a0d41fe6a5495a
Alfardil Alam <fardilfayed@gmail.com>
2025-02-26 01:47:34 -0500

    parsed superjson

--- frontend/src/Menu/_components/auth/hooks.ts
+++ frontend/src/Menu/_components/auth/hooks.ts
@@ -1,18 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import { ValidationResponse } from "../../../validation/types";
+import superjson from "superjson";
 
-async function fetchAuth(): Promise<ValidationResponse> {
+async function fetchAuth(): Promise<any> {
   const res = await fetch("/api/auth/v1/validate", {
     credentials: "include",
   });
   if (!res.ok) {
     throw new Error("Not authenticated.")
   }
-  return res.json();
+  const parsed = superjson.parse(await res.text());
+  return parsed;
 }
 
 export const useFetchAuthQuery = () => {
-    const {data, error, isLoading, } = useQuery<ValidationResponse>({
+    const {data, error, isLoading, } = useQuery<any>({
         queryKey: ["auth", "validate"],
         queryFn: fetchAuth,
     });
